4a163dcad7b32850afe8194727880faa30d215d4
Dilip <dilip.884400@gmail.com>
2023-03-06 22:12:51 +0530

    assignment done

---
+++ src/app2.js
@@ -0,0 +1,38 @@
+export default
+
+
+function Challenge(){
+    return (
+        <>
+        <One/>
+        <Second/>
+        <Third/>
+        <Forth/>
+        </>
+    )
+
+}
+
+function One(){
+    return (
+        <h1>I am heading one....from first component</h1>
+    )
+}
+function Second(){
+    return (
+        <h2>I am heading two.....from second component</h2>
+    )
+}
+function Third(){
+    return (
+        <h3>I am heading three.....from third component</h3>
+    )
+}
+function Forth(){
+    return(
+        <button>click me</button>
+    )
+
+}
+
+
